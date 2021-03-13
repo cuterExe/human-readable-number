2668ea72f7cf84202f4c19349d9f23f031fa22a1
Андрей Жданов <cuter.exe@gmail.com>
2021-03-13 18:55:31 +0300

    refactor: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    var ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    var tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) return "zero";
+
+    if (number < 20) {
+        return ones[number];
+    }
+
+    var numString = number.toString();
+
+    if (numString.length === 2) {
+        const actualTens = tens[numString[0]];
+        return number % 10 === 0
+            ? actualTens
+            : actualTens + " " + ones[numString[1]];
+    }
+
+    if (numString.length === 3) {
+        const actualHundreds = ones[numString[0]] + " hundred";
+        return number % 100 === 0
+            ? actualHundreds
+            : actualHundreds + " " + toReadable(number % 100);
+    }
+};
